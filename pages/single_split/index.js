e3f0ec94318eea92b97f366d62d5f2c38c7f02c5
faldibule <faldibule@gmail.com>
2021-10-03 10:19:32 +0700

    path

---
+++ pages/single_split/index.js
@@ -0,0 +1,108 @@
+const cardTemplate = (value, parent) =>{
+    const divParent = document.createElement('div')
+    divParent.setAttribute('class', 'col-md-4 mt-3 border shadow') 
+    
+    const card = document.createElement('div')
+    card.setAttribute('class', 'card')
+    card.setAttribute('style', 'width: 18rem;')
+
+    const img = document.createElement('img')
+    img.setAttribute('src', `${value.img}`)
+    img.setAttribute('class', 'card-img-top')
+    img.setAttribute('style', 'width: 350px; height: 200px; object-fit: cover; object-position: center;')
+
+    const cardBody = document.createElement('div')
+    cardBody.setAttribute('class', 'card-body text-center')
+
+    const cardTitle = document.createElement('h5')
+    cardTitle.textContent = value.title
+
+    const cardLink = document.createElement('a')
+    cardLink.href = value.link
+    cardLink.textContent = 'Cek Harga Product'
+    cardLink.setAttribute('class', 'btn btn-primary mb-3')
+
+    cardBody.appendChild(cardTitle)
+    cardBody.appendChild(cardLink)
+
+    divParent.appendChild(img)
+    divParent.appendChild(cardBody)
+
+    parent.appendChild(divParent)
+}
+
+
+//PREMIUM SERIES
+const premium = [
+    {
+        title: 'Urusara 7',
+        link: '',
+        img: '../../img/single_split/premium/urusara7.png',
+    },
+    {
+        title: 'Multi Split',
+        link: '',
+        img: '../../img/single_split/premium/euro.png',
+
+    }
+]
+
+const listProductPremium = document.querySelector('#list-product-premium')
+
+premium.forEach((value, i) => {
+    cardTemplate(value, listProductPremium)
+})
+
+
+//Inverter
+const inverter = [
+    {
+        title: 'Premium Inverter',
+        link: '',
+        img: '../../img/single_split/inverter/premium.jpg',
+    },
+    {
+        title: 'Star Inverter',
+        link: '',
+        img: '../../img/single_split/inverter/star.png',
+    },
+    {
+        title: 'Flash Inverter',
+        link: '',
+        img: '../../img/single_split/inverter/flash.png',
+    }
+]
+
+const listProductInverter = document.querySelector('#list-product-inverter')
+inverter.forEach((value, i) => {
+    cardTemplate(value, listProductInverter)
+})
+
+
+
+//Non-Inverter
+const nonInverter = [
+    {
+        title: 'Super Mini Split',
+        link: '',
+        img: '../../img/single_split/non_inverter/super.png',
+    },
+    {
+        title: 'Lite',
+        link: '',
+        img: '../../img/single_split/non_inverter/lite.png',
+    },
+    {
+        title: 'Breeze',
+        link: '',
+        img: '../../img/single_split/non_inverter/breeze.png',
+
+    }
+]
+
+const listProductNonInverter = document.querySelector('#list-product-nonInverter')
+
+
+nonInverter.forEach((value, i) => {
+    cardTemplate(value, listProductNonInverter)
+})
